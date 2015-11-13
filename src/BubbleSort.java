20bab2c7b753ecebf340cbaf085b8d811c7cd7ae
Akhil Subbarao <akhilsubbarao98@gmail.com>
2015-11-12 23:09:13 -0800

    Finished the Bubble & Insertion Sorts
    Now to do the Selection Sort

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,9 +1,57 @@
+import java.util.Scanner;
 
 public class BubbleSort {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private static int[] Array;
 
+	public static void main(String[] args) {
+		Array = getArray();
+		printArray();
+		sort();
+		sort();
+		System.out.println();
+		printArray();
+	}
+
+	public static void sort() {
+		int first = 0;
+		int second = Array.length - 1;
+
+		bubbleSort(first, second);
+	}
+
+	private static void bubbleSort(int first, int second) {
+		for (int i = second; i > 1; i--) {
+			for (int j = first; j < i; j++) {
+				if (Array[j] > Array[j + 1]) {
+					swap(j, j + 1);
+				}
+			}
+		}
+	}
+
+	public static void swap(int left, int right) {
+		int temp = Array[left];
+		Array[left] = Array[right];
+		Array[right] = temp;
+	}
+
+	public static void printArray() {
+		for (int i : Array) {
+			System.out.print(i + " ");
+		}
+	}
+
+	public static int[] getArray() {
+		Scanner scanner = new Scanner(System.in);
+		int[] array = new int[10];
+		int item = 0;
+		for (int i = 0; i < 10; i++) {
+			System.out.println("please enter the " + i + "th number to be sorted.");
+			item = scanner.nextInt();
+			array[i] = item;
+		}
+		return array;
 	}
 
 }
--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -1,9 +1,45 @@
+import java.util.Scanner;
 
 public class InsertionSort {
+	private static int[] Array;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Array = getArray();
+		sort(Array);
+		printArray(Array);
 	}
 
+	public static int[] sort(int[] Array) {
+		for (int i = 1; i < Array.length; i++) {
+			int startPt = Array[i];
+			int j = i - 1;
+
+			while (j >= 0 && Array[j] > startPt) {
+				Array[j + 1] = Array[j];
+				j--;
+			}
+			Array[j + 1] = startPt;
+		}
+		return Array;
+	}
+
+	public static int[] getArray() {
+		Scanner scanner = new Scanner(System.in);
+		int[] array = new int[10];
+		int item = 0;
+		for (int i = 0; i < 10; i++) {
+			System.out.println("please enter the " + i + "th number to be sorted.");
+			item = scanner.nextInt();
+			array[i] = item;
+		}
+		return array;
+	}
+
+	public static void printArray(int[] Array) {
+
+		for (int i : Array) {
+			System.out.print(i + " ");
+
+		}
+	}
 }
